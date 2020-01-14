e5ffd4c375a01d74a4508c95d671898d1dc0ab68
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-01-14 15:23:51 -0500

    basic layout for the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="card border-0 shadow my-5">
+                    <div className="card-body p-5">
+                        <h1 className="font-weight-light">ISP Speed Expectations vs Reality</h1>
+                        <p className="lead">Over several months, I collected speedtest data to compare with what the
+                            Internet Service Provider advertises and the reality of it<br/>This page shows the results
+                            using graphs</p>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col">
+                                    <img className="img-fluid" src="http://placehold.it/750x500" alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
