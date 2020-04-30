31cb7e5938307c3db71cced1e3408353520c5a8b
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-04-30 16:17:37 -0400

    refactor: rewrite scraper into a docker container

---
+++ scraper/healthcheck/healthcheck.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from enum import Enum
+
+import requests
+
+logger = logging.getLogger("speedtest_scraper")
+
+
+class Status(Enum):
+    SUCCESS = ""
+    START = "/start"
+    FAIL = "/fail"
+
+
+class HealthCheck:
+    @staticmethod
+    def ping_status(status: Status):
+        if "DEV_RUN" in os.environ:
+            return
+        if "SCRAPER_HC_UUID" not in os.environ:
+            raise EnvironmentError("Missing SCRAPER_HC_UUID")
+        requests.get(
+            f"https://hc-ping.com/{os.getenv('SCRAPER_HC_UUID')}{status.value}"
+        )
