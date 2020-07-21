ee1a93a91a83e159adb349ed452c9debdd651923
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-07-21 17:57:41 -0400

    ci: build a latest image and a stable image
    - remove notification after

--- .docker/scripts/build.sh
+++ .docker/scripts/build.sh
@@ -4,9 +4,10 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx rm builder || true
 docker buildx create --name builder --driver docker-container --use
 docker buildx inspect --bootstrap
-cd /drone/src/frontend || exit
-docker buildx build -t gordonpn/speedtest-frontend:latest --platform linux/amd64,linux/arm64 --push .
-cd /drone/src/backend || exit
-docker buildx build -t gordonpn/speedtest-backend:latest --platform linux/amd64,linux/arm64 --push .
-cd /drone/src/scraper || exit
-docker buildx build -t gordonpn/speedtest-scraper:latest --platform linux/amd64,linux/arm64 --push .
+DOCKER_TAG=${DOCKER_TAG:-latest}
+cd /drone/src/frontend || exit 1
+docker buildx build -t gordonpn/speedtest-frontend:"$DOCKER_TAG" --platform linux/amd64,linux/arm64 --push .
+cd /drone/src/backend || exit 1
+docker buildx build -t gordonpn/speedtest-backend:"$DOCKER_TAG" --platform linux/amd64,linux/arm64 --push .
+cd /drone/src/scraper || exit 1
+docker buildx build -t gordonpn/speedtest-scraper:"$DOCKER_TAG" --platform linux/amd64,linux/arm64 --push .
